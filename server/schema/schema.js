c1f0170ac201bf044ab0d251189eb67f9c3d243e
saqib masood <saqib_98@hotmail.co.uk>
2022-06-29 11:01:16 +0100

    inital commit

---
+++ server/schema/schema.js
@@ -0,0 +1,69 @@
+const { project, client, clients, projects } = require('../sampleData.js')
+
+const { GraphQLObjectType, GraphQLID, GraphQLString, graphql, GraphQLSchema, GraphQLList } = require('graphql');
+
+//client type
+ 
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: {type: GraphQLID },
+        name: {type: GraphQLString },
+        email: {type: GraphQLString },
+        phone: {type: GraphQLString }
+
+    })
+});
+//project type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: {type: GraphQLID },
+        name: {type: GraphQLString },
+        description: {type: GraphQLString },
+        status: {type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args){
+                return clients.find(client => client.id === parent.clientId);
+            }
+        }
+    
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent,args){
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: {type: GraphQLID }},
+            resolve(parent, args){
+                return projects.find(project => project.id === args.id);
+            }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent,args){
+                return clients;
+            }
+        },
+        client: {
+            type: ClientType,
+            args: { id: {type: GraphQLID }},
+            resolve(parent, args){
+                return clients.find(client => client.id === args.id);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
